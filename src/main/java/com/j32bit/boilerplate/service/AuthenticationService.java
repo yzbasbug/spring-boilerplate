6a5f24d183714bc28f6c3337bf3dfff63c0c9701
yzbasbug <yusuf.basbug@toyotatr.com>
2022-05-31 15:46:12 +0300

    Javadocs added to services

--- src/main/java/com/j32bit/boilerplate/service/AuthenticationService.java
+++ src/main/java/com/j32bit/boilerplate/service/AuthenticationService.java
@@ -3,5 +3,10 @@ package com.j32bit.boilerplate.service;
 import com.j32bit.boilerplate.dto.UserLoginRequestDTO;
 
 public interface AuthenticationService {
+    /**
+     *
+     * @param userLoginRequestDTO
+     * @return generated token to access the other secured resources
+     */
     String authenticate(UserLoginRequestDTO userLoginRequestDTO);
 }
--- src/main/java/com/j32bit/boilerplate/service/UserService.java
+++ src/main/java/com/j32bit/boilerplate/service/UserService.java
@@ -6,7 +6,16 @@ import com.j32bit.boilerplate.entity.User;
 import java.util.ArrayList;
 
 public interface UserService {
+    /**
+     *
+     * @param username
+     * @return the requested user if it is exists.
+     */
     public User findByUsername(String username);
 
+    /**
+     *
+     * @return all Users
+     */
     ArrayList<UserDTO> findAll();
 }
